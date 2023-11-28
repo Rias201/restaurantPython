75590e0187274b9d5d425e097b727c336288d23e
Biel Clarà <115286129+bclara2001@users.noreply.github.com>
2023-11-28 16:26:15 +0100

    QoL database.py update

--- database.py
+++ database.py
@@ -13,6 +13,7 @@ def disconnectdb(conn):
     conn.close()
 
 def afegir_comanda(conn,n_comanda,producte,quantitat):
+    conn = connectdb()
     cursor = conn.cursor()
     try:
         cursor.execute("INSERT INTO comandes VALUES (%s,%s,%s)", (n_comanda,producte,quantitat))
@@ -24,17 +25,22 @@ def afegir_comanda(conn,n_comanda,producte,quantitat):
         return False
     finally:
         cursor.close()
+        disconnectdb(conn)
 
 def recollir_comanda(conn,n_comanda):
+    conn = connectdb()
     cursor = conn.cursor()
     cursor.execute("SELECT producte, quantitat FROM comandes WHERE id = %s", (n_comanda,))
     json = cursor.fetchall()
     cursor.close()
+    disconnectdb(conn)
     return json
 
 def recollir_productes(conn,tipus):
+    conn = connectdb()
     cursor = conn.cursor()
     cursor.execute("SELECT nom, preu, stock FROM productes WHERE tipus = %s", (tipus,))
     llista_productes = cursor.fetchall()
     cursor.close()
+    disconnectdb(conn)
     return llista_productes
